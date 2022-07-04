f3462b1ee7f974ca50385605ba55b7fcff537249
João_Pedro <106271951+jaozinsz@users.noreply.github.com>
2022-07-04 17:46:45 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+var notas = [
+  { id: 1, Nome: "João Pedro", Nota: 8.5 },
+  { id: 2, Nome: "Geovanna", Nota: 9 },
+  { id: 3, Nome: "Caio", Nota: 8 },
+  { id: 4, Nome: "Michele", Nota: 5 },
+  { id: 5, Nome: "Jessica", Nota: 6 },
+  { id: 6, Nome: "Flavio", Nota: 3.5 }
+];
+
+var Nomes = notas.map((notas) => notas.Nome);
+var nota = notas.map((notas) => notas.Nota);
+var id = notas.map((notas) => notas.id);
